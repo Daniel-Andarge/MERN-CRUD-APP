922a9a4b4321b2d0de64042a7a361859634728d1
Daniel-Andarge <andargedaniel90@gmail.com>
2023-08-03 21:31:20 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React, { Component } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "@fortawesome/fontawesome-free/css/all.css";
-import "@fortawesome/fontawesome-free/js/all.js";
-import AddTutorial from "./components/AddTutorial";
-import Tutorial from "./components/TutorialsList";
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
+import './App.css';
+
+import AddTutorial from './components/AddTutorial';
+import Tutorial from './components/Tutorial';
+import TutorialsList from './components/TutorialsList';
 
 function App() {
   return (
     <div>
       <nav className="navbar navbar-expand navbar-dark bg-dark">
         <a href="/tutorials" className="navbar-brand">
-         E-Learning
+          E-Learning
         </a>
         <div className="navbar-nav mr-auto">
           <li className="nav-item">
-            <Link to={"/tutorials"} className="nav-link">
+            <Link to={'/tutorials'} className="nav-link">
               Tutorials
             </Link>
           </li>
           <li className="nav-item">
-            <Link to={"/add"} className="nav-link">
+            <Link to={'/add'} className="nav-link">
               Add
             </Link>
           </li>
@@ -28,7 +32,7 @@ function App() {
 
       <div className="container mt-3">
         <Switch>
-          <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
+          <Route exact path={['/', '/tutorials']} component={TutorialsList} />
           <Route exact path="/add" component={AddTutorial} />
           <Route path="/tutorials/:id" component={Tutorial} />
         </Switch>
@@ -36,4 +40,5 @@ function App() {
     </div>
   );
 }
+
 export default App;
